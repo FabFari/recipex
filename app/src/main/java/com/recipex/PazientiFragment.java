b0d92f493ab761fccf598b93984de24967bb76d7
Sara Veterini <saraveterini@gmail.com>
2016-04-24 16:46:41 +0200

    fragments and drawer layout

---
+++ app/src/main/java/com/recipex/PazientiFragment.java
@@ -0,0 +1,21 @@
+package com.recipex;
+
+/**
+ * Created by Sara on 24/04/2016.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class PazientiFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pazienti_layout,null);
+    }
+}
